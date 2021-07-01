24e2fcb962f614a22473fb4296ef1e8a68f27ba1
Frederico Meira <fmeira@c2r7s1.42lisboa.com>
2021-07-02 00:30:58 +0100

    PUSH_SWAP DONEEE. Checker & norm tomorrow

--- solve_b.c
+++ solve_b.c
@@ -1,20 +1,5 @@
 #include "push_swap.h"
 
-static	t_sizes	*init_r_struct(void)
-{
-	t_sizes		*new;
-
-	new = (t_sizes *)malloc(sizeof(t_sizes));
-	if (!new)
-		terminate();
-
-	new->rx = 0;
-	new->rrx = 0;
-	new->direction = R;
-
-	return (new);
-}
-
 static t_shift_info	*init_shift_info(void)
 {
 	t_shift_info	*shift_info;
@@ -29,6 +14,7 @@ static t_shift_info	*init_shift_info(void)
 	shift_info->b_direction = R;
 	shift_info->size = 0;
 	shift_info->is_set = false;
+	return (shift_info);
 }
 
 static void			move_b(t_stack *a_stack,
@@ -42,20 +28,26 @@ static void			move_b(t_stack *a_stack,
 		if (shift_info->a_direction == shift_info->b_direction
 			&& shift_info->a_elem != a_stack->head
 			&& shift_info->b_elem != b_stack->head)
+		{
 			if (shift_info->a_direction == R)
 				rr(a_stack, b_stack, "rr", command_list);
 			else
 				rrr(a_stack, b_stack, "rrr", command_list);
+		}
 		else if (shift_info->a_elem != a_stack->head)
+		{
 			if (shift_info->a_direction == R)
 				rx(a_stack, "ra", command_list);
 			else
 				rrx(a_stack, "rra", command_list);
+		}
 		else if (shift_info->b_elem != b_stack->head)
+		{
 			if (shift_info->b_direction == R)
 				rx(b_stack, "rb", command_list);
 			else
 				rrx(b_stack, "rrb", command_list);
+		}
 	}
 }
 
